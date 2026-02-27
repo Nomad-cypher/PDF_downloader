bc9100350b07903e5740f52d3c717578b5efd87c
Nomad-cypher <258812503+Nomad-cypher@users.noreply.github.com>
2026-02-27 14:42:34 +0100

    Just finished I am not happy with where I am at, but I've had to use my time to delete all previous repos because they showed my email address, and I was NOT happy about. This also means that I lost all my contributions from my previous weeks :( So yeah, I'm not happy with GitHub right now...

--- PDF_downloader.py
+++ PDF_downloader.py
@@ -6,23 +6,18 @@
 # Remember to let the r before the string stay so that Python knows it's a raw string!
 excel_file_path = r"C:\Users\Spac-23\Documents\Opgaver - Softwarespor\Uge 4\GRI_2017_2020 (1) (test version).xlsx"
 
+# Specify the name of the column that the reports should be named after
+name_column = "BRnum"
+
 # Specify which columns in the excel file to locate the download links
-# Value should be a list, and each element can be either:
-# 1: A combination of uppercase letters, such as B, AM, or BL, corrosponding to the columns in the excel sheet
-# 2: Positive integers, such as 2, 14, or 145
+# Value should be headers of the sheet
 # The columns are prioritized, the program will start by checking the first column and then go from there
-download_link_columns = ["AL", "AM"] #TODO: ændres
+download_link_columns = ['Pdf_URL', 'Report Html Address']
 
 # Specify a column in the excel file for the program to write whether ad PDF file has been downloaded or not
-# Value can be either:
-# 1: A combination of uppercase letters, such as B, AM, or BL, corrosponding to the columns in the excel sheet
-# 2: A positive integer, such as 2, 14, or 145
+# Value should be a header of the sheet
 # Note that a column should be created in the excel file for the purpose of this, if one is not already created
-is_downloaded_column = "Is downloaded?" #TODO: ændres
-
-# Specify whether the data in the excel sheet contains a header
-# Value should be either True or False
-contains_header = True #TODO: Fjernes?
+is_downloaded_column = "Is downloaded?"
 
 # Specify the path and folder to place the downloaded files
 # Can be done by selecting the folder and then using Ctrl + Shift + C
@@ -75,24 +70,44 @@ if __name__ == "__main__":
     # Load the excel file into a data frame
     dataframe = pandas.read_excel(excel_file_path)
 
-    print(dataframe['BRnum'])
-    print(dataframe['BRnum'][3])
-    print(dataframe['Report Html Address'][18599-2])
-    print(dataframe.shape[0])
-
-    filename = str(destination_folder + "/test.pdf") #TODO: write comments and make sure it writes a proper filename
-    response = requests.get(dataframe['Report Html Address'][18599-2])
-    with open(filename,'wb') as file:
-        file.write(response.content)
-
     # Define iteration variable and find the number of rows in the dataset
     downloads = 0 # Keep track of the number of successful downloads
     max = dataframe.shape[0]
 
     # Looping through the data frame
     # The loop should exit when there's no more data
-    while i < max: #TODO: make for loop instead, also make number of successfull downloads a different variable
+    for i in range(dataframe.shape[0]):
+        # Skip if already downloaded
+        if dataframe[is_downloaded_column][i] == True:
+            continue
+        
+        # Provide a file path and name for the report
+        filename = str(destination_folder + "/" + dataframe[name_column][i] + ".pdf")
+        # Attempt to download from the provided columns, if unsuccessful, skip
+        try:
+            for j in download_link_columns:
+                try:
+                    # Download and save file from link
+                    response = requests.get(dataframe[j][i])
+                    with open(filename,'wb') as file:
+                        file.write(response.content)
+                    
+                    # Mark as downloaded
+                    is_downloaded_indecies.append(i)
 
-
-######## TIL TESTNING: Følgende adresse giver faktisk en PDF fil: https://www.tateandlyle.com/sites/default/files/2019-06/tatelyle-annual-report-2019-interactive-pdf.pdf
-# Ligger som udgangspunkt i række 18599
+                    # No need to try more if this one returns a successful download
+                    continue
+                except:
+                    raise Exception
+        except:
+            continue
+        
+        # If the program runs in restricted mode and max downloads is reached, exit the loop
+        if restricted_mode:
+            downloads += downloads + 1
+            if downloads >= max_downloads:
+                break
+    
+    # Write to the excel file to save the downloaded markings
+    # Yes, I'm aware this is not very much not the most efficient way to do it
+    #dataframe.to_excel(excel_file_path)
